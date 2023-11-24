278b4ce95f01531538028159e6053747b12cd79e
akio0911 <akio0911@gmail.com>
2023-11-24 21:34:21 +0900

    feedback

--- tomoEng11Kadai2/ViewController.swift
+++ tomoEng11Kadai2/ViewController.swift
@@ -13,38 +13,32 @@ class ViewController: UIViewController {
     @IBOutlet private weak var label: UILabel!
     @IBOutlet private weak var calculateSegment: UISegmentedControl!
 
-    var selectedSegmentNumber: Int?
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
         textField1.keyboardType = .numberPad
         textField2.keyboardType = .numberPad
     }
+
     @IBAction func caluculateButtonPressed(_ sender: UIButton) {
 
-        if let num1 = Double(textField1.text!), let num2 = Double(textField2.text!) {
+        guard let num1 = Double(textField1.text!), let num2 = Double(textField2.text!) else { return }
 
-            switch selectedSegmentNumber {
-            case 0:
-                label.text = String(num1 + num2)
-            case 1:
-                label.text = String(num1 - num2)
-            case 2:
-                label.text = String(num1 * num2)
-            case 3:
-                if num2 != 0 {
-                    label.text = String(num1 / num2)
-                } else {
-                    label.text = "割る数には0以外の数字を入力してください"
-                }
-            default:
-                label.text = String(num1 + num2)
+        switch calculateSegment.selectedSegmentIndex {
+        case 0:
+            label.text = String(num1 + num2)
+        case 1:
+            label.text = String(num1 - num2)
+        case 2:
+            label.text = String(num1 * num2)
+        case 3:
+            if num2 != 0 {
+                label.text = String(num1 / num2)
+            } else {
+                label.text = "割る数には0以外の数字を入力してください"
             }
+        default:
+            label.text = String(num1 + num2)
         }
-    }
-    @IBAction func caluculateSegmentPressed(_ sender: UISegmentedControl) {
-
-        selectedSegmentNumber = calculateSegment.selectedSegmentIndex
     }
 }
